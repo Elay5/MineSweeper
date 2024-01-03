b22a88273059199e6bdaf6bfdb3adb4a07258267
Elay5 <155320852+Elay5@users.noreply.github.com>
2024-01-03 21:12:24 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function onInit() {
 
     gBoard = buildBoard()
     renderBoard(gBoard)
-    
+
 }
 
 function buildBoard() {
@@ -36,11 +36,30 @@ function buildBoard() {
                 isMarked: true,
 
             }
-            
+
         }
     }
-    board[1][2].isMine = true
-    board[3][3].isMine = true
+
+    // var mines = []                   // randomizing mines
+    // var mineCounter = 0
+    // while (mineCounter < gLevel.MINES) {
+
+    //     var randomRow = getRandomInt(0, gLevel.SIZE)
+    //     var randomCol = getRandomInt(0, gLevel.SIZE)
+
+    //     if (!mines.includes(`${randomRow}-${randomCol}`)) {   // if mine arrays does Not include.
+
+    //         board[randomRow][randomCol].isMine = true
+    //         mines.push(`${randomRow}-${randomCol}`)
+    //         mineCounter++
+    //     }
+    //     else continue
+    // }
+
+
+    board[getRandomInt(0, gLevel.SIZE)][getRandomInt(0, gLevel.SIZE)].isMine = true
+    board[getRandomInt(0, gLevel.SIZE)][getRandomInt(0, gLevel.SIZE)].isMine = true
+
     setMinesNegsCount(board)
     return board
 }
@@ -60,32 +79,105 @@ function setMinesNegsCount(board) {
                     if (board[k][l].isMine) currCell.minesAroundCount++
 
                 }
-
             }
-            
 
         }
     }
-    
+
     return
-   
-}
-
-
-function onCellClicked(elCell, i, j){
-const currCell=document.querySelector(`.cell-${i}-${j} span`)
-
-// elCell.innerText=
 
 }
 
 
-function gameLevel(level) {
+function onCellClicked(elCell, i, j) {
+    
+if(!gGame.isOn)return
+    const currCell = gBoard[i][j]
+    const elCurrCell = document.querySelector(`.cell-${i}-${j}`)
+    if (currCell.isShown) return
+
+    else {
+        currCell.isShown = true
+        if (currCell.isMine) {
+            elCell.innerText = MINE
+            elCurrCell.classList.add('clickedCell')
+
+            const elLose = document.querySelector('.lose')
+            const elTable = document.querySelector('table')
+            elLose.style.display = 'block'
+            gGame.isOn=false
+            
+        }
+        else if (currCell.minesAroundCount > 0) {
+            elCell.innerText = currCell.minesAroundCount
+            elCurrCell.classList.add('clickedCell')
+
+        }
+        else {
+            elCell.innerText = ' '
+
+            //negloop if 0 mines
+            for (var k = i - 1; k <= i + 1; k++) {
+                if (k < 0 || k >= gLevel.SIZE) continue
+                for (var l = j - 1; l <= j + 1; l++) {
+                    if (l < 0 || l >= gLevel.SIZE) continue
+                    if (i === k && j === l) continue
+                    const negCell = gBoard[k][l]
+                    negCell.isShown = true
+                    const elNegCell = document.querySelector(`.cell-${k}-${l}`)
+
+                    elNegCell.innerText = negCell.minesAroundCount ? negCell.minesAroundCount : ' '
+                    elNegCell.classList.add('clickedCell')
+                    elCurrCell.classList.add('clickedCell')
+
+                }
+            }
+        }
+    }
+    checkWin()
+}
+
+function gameLevel(level, mineMult) {
     gLevel.SIZE = 4
+    gLevel.MINES = 2
+
     gLevel.SIZE *= level
+    gLevel.MINES *= mineMult
     onInit()
 
 }
+
+function onCellMarked(elCell) {
+
+}
+
+function checkWin() {
+    for (var i = 0; i < gLevel.SIZE; i++) {
+        for (var j = 0; j < gLevel.SIZE; j++) {
+            var currCell = gBoard[i][j]
+            if (!currCell.isShown) return
+        }
+    }
+
+    const elWin = document.querySelector('.win')
+    const elTable = document.querySelector('table')
+    elWin.style.display = 'block'
+    elTable.style.display = 'none'
+    console.log('hi')
+
+}
+
+function startGame(){
+    gGame.isOn=true
+    const elLose = document.querySelector('.lose')
+    elLose.style.display = 'none'
+
+    onInit()
+
+}
+
+
+
 
 
 
--- utils.js
+++ utils.js
@@ -6,25 +6,35 @@ function renderBoard(board) {
     for (var i = 0; i < gLevel.SIZE; i++) {
         strHtml += `<tr>\n`
         for (var j = 0; j < gLevel.SIZE; j++) {
-            if (!board[i][j].isMine) {
-                strHtml += `
-               \t<td>
-                    <button class="btn cell cell-${i}-${j}"
-                     onclick="onCellClicked(this,${i},${j})"
-                     ><span>${board[i][j].minesAroundCount}</span></button>
-                </td>\n`
-            }
-            else {
+            if (!board[i][j].isShown) {
                 strHtml += `
                 \t<td>
-                     <button class="btn cell cell-${i}-${j}"
-                     onclick="onCellClicked(this,${i},${j})"
-                     ><span class="hidden">${MINE}</span></button>
+                     <button class="cell cell-${i}-${j}"
+                      onclick="onCellClicked(this,${i},${j})"
+                      > </button>
                  </td>\n`
+
+            }
+            else {
+
+                if (!board[i][j].isMine) {
+                    strHtml += `
+                    \t<td>
+                         <button class="cell cell-${i}-${j}"
+                          onclick="onCellClicked(this,${i},${j})"
+                          >${board[i][j].minesAroundCount}</button>
+                     </td>\n`
+                }
+                else {
+                    strHtml += `
+                     \t<td>
+                          <button class="cell cell-${i}-${j}"
+                          onclick="onCellClicked(this,${i},${j})"
+                          >${MINE}</button>
+                      </td>\n`
+                }
             }
         }
-            
-        
         strHtml += `</tr>\n`
     }
     strHtml += '<table><tbody>'
@@ -35,6 +45,8 @@ function renderBoard(board) {
 
 
 
-
-
-// onclick="cellClicked(this,${num})"> ${num} 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
